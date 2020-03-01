79169b46e3f0b2f9ff522845f3689ce223923174
Peter Örneholm <peter@orneholm.com>
2020-03-01 09:07:56 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# iot-samples
-Samples for IoT services in Azure.
+# Azure IoT Samples
+
+[![Twitter Follow](https://img.shields.io/badge/Twitter-@PeterOrneholm-blue.svg?logo=twitter)](https://twitter.com/PeterOrneholm)
---
+++ src/Orneholm.IotSamples.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Orneholm.IotSamples.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
